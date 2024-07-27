99ef3d859cb8bb2d277a8d590a9a046b71bc272f
Jose Latines <joselatinestranslator@gmail.com>
2024-07-27 09:02:54 -0400

    add

--- src/app/api/portfolios/route.ts
+++ src/app/api/portfolios/route.ts
@@ -82,11 +82,30 @@ export async function GET(req: Request) {
 }
 
 export async function POST(req: Request) {
-	const browser = await puppeteer.launch({ headless: false });
+	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 
 	// Navigate the page to a URL.
 	await page.goto("https://developer.chrome.com/");
+
+	await page.setViewport({width: 1080, height: 1024});
+
+// Type into search box.
+await page.locator('.devsite-search-field').fill('automate beyond recorder');
+
+// Wait and click on first result.
+await page.locator('.devsite-result-item-link').click();
+
+// Locate the full title with a unique string.
+const textSelector = await page
+  .locator('text/Customize and automate')
+  .waitHandle();
+const fullTitle = await textSelector?.evaluate(el => el.textContent);
+
+// Print the full title.
+console.log('The title of this blog post is "%s".', fullTitle);
+
+await browser.close();
 	const data = await req.json();
 	const session = await getServerSession(authConfig);
 
